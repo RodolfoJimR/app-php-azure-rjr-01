7f7be861b7e5f6e44f8e16bc84d9c0c6460df997
Rodolfo Jiménez <93786166+RodolfoJimR@users.noreply.github.com>
2021-11-06 15:58:57 -0600

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 $port = 1433;
-$serverName = "miservidor01.database.windows.net" . $port;
+$serverName = "miservidor01.database.windows.net," . $port;
 $database = "AdventureWorks";
 $userName = "Student";
 $password = "Pa55w.rd";
