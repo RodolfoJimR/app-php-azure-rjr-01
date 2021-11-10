d2332fc88f8aae6c593a92e200deda9afe908cf1
Rodolfo Jiménez <93786166+RodolfoJimR@users.noreply.github.com>
2021-11-09 19:04:53 -0600

    Update select.php

--- select.php
+++ select.php
@@ -1,23 +1,22 @@
 <?php
 $port = 1433;
-$serverName = "tcp:YOUR_SQL_AZURE_URL," . $port;
-$database = "mySQLDataBase";
-$userName = "student-azure";
-$password = "myServerSQL#";
+$serverName = "tcp:examserver01.database.windows.net," . $port;
+$database = "RA-DB-MOVIES";
+$userName = "Student";
+$password = "Ra.exam3n";
 
 try {
     $conn = new PDO("sqlsrv:server = $serverName,$port; Database = $database", $userName, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'SELECT id, firstname, lastname, email, reg_date FROM MyGuests ORDER BY lastname';
+    $sql = 'SELECT id, nombre, lanzamiento, lenguaje FROM pelicula';
 
     foreach ($conn->query($sql) as $row) {
         echo $row['id'] . " | ";
-        echo $row['firstname'] . " | ";
-        echo $row['lastname'] . " | ";
-        echo $row['email'] . " | ";
-        echo $row['reg_date'] . "<br>";
+        echo $row['nombre'] . " | ";
+        echo $row['lanzamiento'] . " | ";
+        echo $row['lenguaje'] . "<br>";
     }
 
     // use exec() because no results are returned
